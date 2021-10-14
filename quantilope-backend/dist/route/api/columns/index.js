276e9a47eb724338e1a52407271b3b830d88fade
Ahmed Bakhabou <ahmedbakhabou@Ahmeds-MacBook-Pro.local>
2021-10-14 13:16:56 +0200

    deleteing /folder

--- quantilope-backend/dist/route/api/columns/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/');
-exports.default = { router };
-// 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
-//# sourceMappingURL=index.js.map
